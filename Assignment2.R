a8306688f2ed3c4ba55f4e839d1092763de3c645
DannyGsGit <danny.godbout@gmail.com>
2016-06-27 18:09:40 -0700

    Initial commit of DS350 project and empty Assignment2.R file.

---
+++ Assignment2.R
@@ -0,0 +1,9 @@
+#### Assignment 2 ####
+# Prepared By: Danny Godbout
+
+# Goal: 
+# Write an R-script to compute the Monty Hall Probabilities with
+# simulations (get probabilities AND variances for switching and
+# not switching).
+
+##
